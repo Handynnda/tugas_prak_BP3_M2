4acce64f41cb419b214718dad5e3fc122f0a4160
Handy Nanda Fachrizal <Handynandaf@gmail.com>
2024-11-03 22:29:12 +0700

    menambahkan fungsi main login

--- app/src/main/java/com/example/tugas_prak_bp3_m2/MainActivity.java
+++ app/src/main/java/com/example/tugas_prak_bp3_m2/MainActivity.java
@@ -1,24 +1,46 @@
 package com.example.tugas_prak_bp3_m2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText inputUsername, inputPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Menghubungkan elemen UI dengan variabel
+        inputUsername = findViewById(R.id.inputLogin);
+        inputPassword = findViewById(R.id.editTextTextPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        // Menambahkan fungsi login
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = inputUsername.getText().toString();
+                String password = inputPassword.getText().toString();
+
+                // Mengecek apakah username dan password benar
+                if (username.equals("admin") && password.equals("admin123")) {
+                    Toast.makeText(MainActivity.this, "Login Berhasil!", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(MainActivity.this,
+                                DashboardActivity.class);
+                        startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Username atau Password Salah", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
